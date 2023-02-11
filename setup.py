61e20477a9081d23e76b3c1516058db7542358c9
augustepoiroux <augustepoiroux@gmail.com>
2023-02-11 15:07:40 +0100

    🐛 Partial fix of packaging issues

--- setup.py
+++ setup.py
@@ -1,20 +1,15 @@
 from setuptools import setup, Extension, find_packages
 
-from Cython.Distutils import build_ext
-
 setup(
-    cmdclass={"build_ext": build_ext},
     ext_modules=[
         Extension(
             name="pridict",
             sources=[
-                "src/pridict/cypridict.pyx",
-                "src/pridict/array.c",
-                "src/pridict/maxheap.c",
+                "pridict/cypridict.c",
             ],
         )
     ],
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-    package_data={"pridict": ["*.h", "*.pxd", "py.typed", "*.pyi"]},
+    packages=["pridict"],
+    # package_dir={"": "src"},
+    package_data={"pridict": ["*.h", "py.typed", "*.pyi"]},
 )
